bfba269f8c930c64c73d42e221c6dde3729b9629
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-19 08:13:27 +0000

    ok

---
+++ TestFolder/Hallo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hallo{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Geben Sie eine Zahl ein:");
+        zahl = input.nextInt();
+
+        if(zahl == 10){
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("bye!!");
+        }
+            
+    }
+}
